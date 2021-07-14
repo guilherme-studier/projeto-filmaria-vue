1d4b25daad4bbce73c383361fb27ed8d778ca7f0
Thiago <thiago.studier9@gmail.com>
2021-07-13 23:35:52 -0300

    update-filmaria

---
+++ src/components/Loading.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="loading">
+      <h1>Carregando dados...</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    h3{
+        font-size: 25px;
+    }
+</style>
